5db1df0fa4c3bb7ad77848bad59cad9777cbddff
iJornez <hayatodelk@gmail.com>
2023-12-05 15:55:34 -0500

    cambios

--- src/views/devolucion/DevolucionDisp.vue
+++ src/views/devolucion/DevolucionDisp.vue
@@ -1,319 +1,619 @@
 <template>
     <div>
+
         <v-card class="mx-auto" max-width="890" elevation="15" style="margin-top: 50px;">
+
             <v-card-title class="crear" style="margin-left: 37%;">
+
                 <h3> Devolucion de Equipos </h3>
+
             </v-card-title>
 
+
+
             <hr>
+
             <br>
+
             <v-card-text>
+
                 <v-row>
+
                     <v-col cols="10">
+
                         <v-autocomplete v-model="cedulaSeleccionada" dense filled rounded solo class="buscar"
                             :no-data-text="'No hay resultados'" :items="cedulauser" item-text="cedula" item-value="cedula"
                             label="CEDULA">
+
                         </v-autocomplete>
+
                     </v-col>
 
+
+
                     <v-col cols="2">
+
                         <v-btn class="mx-6" width="50px" style="margin-top: -8px;" height="50px" fab dark large
                             color="primary" @click="buscar">
+
                             <v-icon dark>
+
                                 search
+
                             </v-icon>
+
                         </v-btn>
+
                     </v-col>
+
                 </v-row>
+
             </v-card-text>
 
 
 
+
+
+
+
         </v-card>
+
         <br>
+
         <br>
+
+
 
         <v-card class="mx-auto" max-width="890" v-if="tablaPrestamo" elevation="15">
+
             <hr>
+
             <center>
+
                 <h5>Prestamos</h5>
+
             </center>
+
             <v-container>
+
                 <v-data-table :headers="headers" :items="DetallesPrestamos" class="tbl">
+
                     <template v-slot:item.codigo="{ item }">
+
                         {{ item.codigo }}
+
                     </template>
+
                     <template v-slot:item.fechaInicio="{ item }">
+
                         {{ formatearFechas(item.fechaInicio) }}
+
                     </template>
+
                     <template v-slot:item.fechaFin="{ item }">
+
                         {{ formatearFechas(item.fechaFin) }}
+
                     </template>
+
                     <template v-slot:item.tipoEquipo="{ item }">
+
                         {{ item.tipoEquipo }}
+
                     </template>
+
+
 
                     <template v-slot:item.serial="{ item }">
+
                         {{ item.serial }}
+
                     </template>
+
+
 
                     <template v-slot:item.estadoEquipo="{ item }">
+
                         {{ item.estadoEquipo }}
+
                     </template>
 
-                    
+
+
+
+
+
+
+
 
 
 
                     <template>
+
                         <v-select class="select"></v-select>
+
                     </template>
+
+
+
+
 
 
 
                     <template v-slot:item.accion="" v-slot:activator="{ on, attrs }">
+
                         <v-icon small class="mr-2" v-on="on" v-bind="attrs"
                             @click="dialog = true">mdi-text-box-search</v-icon>
+
                     </template>
+
+
 
                 </v-data-table>
 
+
+
             </v-container>
+
             <br>
 
+
+
         </v-card>
+
         <br>
+
         <br>
+
         <div class="text-center" v-if="tablaPrestamo">
+
             <v-dialog v-model="dialog" width="500">
 
+                <v-card class="descripcion">
 
-                <v-card>
                     <v-card-title class="text-h5 grey lighten-2">
-                        Privacy Policy
+
+                        Descripción
+
                     </v-card-title>
 
-                    <v-card-text>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-                        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                        culpa qui officia deserunt mollit anim id est laborum.
-                    </v-card-text>
+                    <center>
 
-                    <v-divider></v-divider>
+                        <v-textarea style="width: 400px; " v-model="descripcion" label="escriba aquí"></v-textarea>
+
+                    </center>
 
                     <v-card-actions>
+
                         <v-spacer></v-spacer>
+
                         <v-btn color="primary" text @click="dialog = false">
-                            I accept
+
+                            Cerrar
+
                         </v-btn>
+
+                        <v-btn color="blue darken-1" text @click="confirmarEdit">
+
+                            Confirmar
+
+                        </v-btn>
+
                     </v-card-actions>
+
                 </v-card>
+
             </v-dialog>
+
         </div>
+
         <v-dialog v-model="dialog" max-width="500px">
+
             <v-card>
+
                 <v-card-title>
+
                     <span class="text-h5">{{ }}</span>
+
                 </v-card-title>
 
+
+
                 <v-card-text>
+
                     <v-container>
+
                         <v-row>
 
+
+
                             <v-col cols="12">
+
                                 <v-select v-model="estadosDelPrestamo.estadoEquipo" :items="estadosDelPrestamo"
                                     item-text="Estado" item-value="id" label="Estado" required dense outlined></v-select>
+
                             </v-col>
 
+
+
                         </v-row>
+
                     </v-container>
+
                 </v-card-text>
 
+
+
                 <v-card-actions>
+
                     <v-spacer></v-spacer>
+
                     <v-btn color="red darken-1" text @click="cancel">
+
                         Cancel
+
                     </v-btn>
+
                     <v-btn color="blue darken-1" text @click="confirmarEdit">
+
                         Confirmar
+
                     </v-btn>
+
                 </v-card-actions>
+
             </v-card>
+
         </v-dialog>
+
     </div>
 </template>
+
       
+
 <script>
+
 import axios from 'axios';
+
 import Swal from "sweetalert2";
+
 export default {
+
     data: () => ({
 
 
 
 
+
+
+
+
+
         headers: [
+
             { text: 'Código', value: 'codigo' },
+
             { text: 'Fecha Inicio', value: 'fechaInicio' },
+
             { text: 'Fecha Fin', value: 'fechaFin' },
+
             { text: 'Tipo', value: 'tipoEquipo' },
+
             { text: 'Serial', value: 'serial' },
+
             { text: 'Estado', value: 'estadoEquipo' },
+
             { text: 'Bueno', value: 'bueno' },
+
             { text: 'Malo', value: 'malo' },
+
             { text: 'Descripcion', value: 'accion' },
+
         ],
+
         data: [],
+
         tablaPrestamo: false,
+
         tablaDetalle: false,
+
         dialog: false,
+
+        checkbox2: false,
+
+
+
         cedulaSeleccionada: null,
+
         cedulauser: [],
+
         DetallesPrestamos: [],
+
         estadosDelPrestamo: [],
+
         prestamos: [],
 
+
+
     }),
+
     watch: {
+
         dialog(val) {
+
             val || this.close()
+
         },
+
         dialogDelete(val) {
+
             val || this.closeDelete()
+
         },
+
     },
+
+
 
     methods: {
 
 
+
+
+
         async buscar() {
+
             if (this.cedulaSeleccionada) {
+
                 try {
+
                     const response = await axios.get('http://localhost:62000/prestamos/obtener_prestamo/' + this.cedulaSeleccionada);
+
                     this.prestamos = response.data;
+
                     if (this.prestamos.length === 0) {
+
                         this.tablaDetalle = false;
+
                         this.cedulaSeleccionada = null;
+
                         Swal.fire('La cedula seleccionada no tiene prestamos asignados!', '', 'error');
+
                     } else {
+
                         this.tablaPrestamo = true;
+
                         this.prestamos.forEach(prestamo => {
+
                             prestamo.prestamo_detalle.forEach(detalle => {
+
                                 const detallePrestamo = {
+
                                     codigo: detalle.equipo.codigo,
+
                                     serial: detalle.equipo.serial,
+
                                     fechaInicio: detalle.fecha_prestamo,
+
                                     fechaFin: detalle.fecha_devolucion,
+
                                     tipoEquipo: detalle.equipo.tipo.tipo,
+
                                     estadoEquipo: prestamo.estado_prestamo.Estado
+
                                 };
+
                                 this.DetallesPrestamos.push(detallePrestamo);
+
                             });
+
                         });
+
                         console.log(this.DetallesPrestamos);
+
                     }
+
                 } catch (error) {
+
                     console.error('Error al obtener detalles de préstamo:' + error);
+
                     Swal.fire('Error al obtener detalles de préstamo', '' + error, 'error');
+
                 }
 
-               
+
+
+
+
             } else {
+
                 Swal.fire('No ha seleccionado una cedula', '', 'error');
+
             }
 
 
+
+
+
         },
+
+
 
         async prestamocedula() {
+
             await axios.get('http://localhost:62000/usuarios/obtener').then(resp => {
+
                 this.cedulauser = resp.data;
+
                 if (this.cedulauser.length > 0) {
+
                     this.cedulaSeleccionada = this.cedulauser.cedula;
+
                 }
+
             });
-        },
-        async estadosprestamos() {
-            await axios.get('http://localhost:62000/estadoprestamo/obtener').then(resp => {
-                this.estadosDelPrestamo = resp.data;
-                console.log(this.estadosDelPrestamo);
-            });
+
         },
 
-        
+        async estadosprestamos() {
+
+            await axios.get('http://localhost:62000/estadoprestamo/obtener').then(resp => {
+
+                this.estadosDelPrestamo = resp.data;
+
+                console.log(this.estadosDelPrestamo);
+
+            });
+
+        },
+
+
+
+
+
+
 
         MostrarDetalles() {
+
             this.tablaDetalle = true;
+
         },
+
+
 
         EditarEstado() {
+
             this.dialog = true;
-        },
-        cancel() {
-            this.dialog = false;
-        },
-        async confirmarEdit() {
-        },
-        formatearFechas(date) {
-            const Fechasformateadas = new Date(date).toLocaleString();
-            return Fechasformateadas;
+
         },
 
+        cancel() {
+
+            this.dialog = false;
+
+        },
+
+        async confirmarEdit() {
+
+        },
+
+        formatearFechas(date) {
+
+            const Fechasformateadas = new Date(date).toLocaleString();
+
+            return Fechasformateadas;
+
+
+
+        },
+
+
+
     },
+
     mounted() {
+
         this.prestamocedula();
+
         this.estadosprestamos();
+
     },
+
 };
+
 </script>
+
       
+
 <style scoped>
 .mx-auto {
+
     font-family: 'Arial Narrow', Arial, sans-serif
 }
 
+
+
 .buscar {
+
     margin-left: 20px;
 
+
+
 }
+
+
 
 .mi-input {
 
+
+
     border: 2px solid #a3a3a3;
+
     border-radius: 5px;
+
     padding-right: 20px;
+
 }
+
+
 
 .mi-boton-nuevo {
+
     margin-bottom: 20px;
+
     margin-right: 400px;
+
     background-color: #276fae;
+
     color: #ffffff;
+
     padding: 8px 20px;
+
     cursor: pointer;
+
     border-radius: 10px;
+
     margin-left: 60px;
 
+
+
 }
+
+
 
 .mi-boton-nuevo2 {
+
     background-color: rgb(10, 129, 66);
+
     color: #ffffff;
+
     padding: 5px 20px;
+
     cursor: pointer;
+
     border-radius: 10px;
+
     margin-left: 80%;
+
     font-size: 20px;
+
 }
+
+
 
 h5 {
+
     padding: 0px 20px;
+
+
 
 }
 
+
+
 .tbl {
+
     background-color: #f2f2f2;
+
 }
 </style>  
--- src/views/welcome.vue
+++ src/views/welcome.vue
@@ -1,48 +1,98 @@
 <template>
-  
   <div class="contenedor">
-   <div class="loan-form">
-     <h1><b>Bienvenido a nuestra plataforma de préstamo de dispositivos</b></h1>
-     <hr>
-     <p>
-Nos enorgullece ofrecer una solución innovadora para aquellos que buscan acceder temporalmente a la última tecnología . Ya sea que necesites un portátil para un proyecto urgente, una tablet para tus estudios o cualquier otro dispositivo electrónico, estamos aquí para cubrir tus necesidades. Nuestra plataforma facilita el proceso de préstamo, conectando a prestamistas y prestatarios de manera segura y eficiente. Explora nuestra amplia gama de dispositivos disponibles y descubre cómo podemos hacer que la tecnología esté al alcance de todos. ¡Bienvenido a la revolución del acceso electrónico!</p>
-     <!-- Agrega aquí tus campos de formulario, botones, etc. -->
-   </div>
- </div>
 
+    <center>
+
+      <div class="loan-form">
+
+        <v-card class="mx-auto" max-width="890" elevation="24" style="margin-top: 5px; ;">
+
+          <br><br>
+
+          <h1><b>Bienvenido a nuestra plataforma de préstamo de dispositivos</b></h1>
+
+          <hr>
+
+          <p>
+
+            Nos enorgullece ofrecer una solución innovadora para aquellos que buscan acceder temporalmente a la última
+            tecnología . Ya sea que necesites un portátil para un proyecto urgente, una tablet para tus estudios o
+            cualquier otro dispositivo electrónico, estamos aquí para cubrir tus necesidades. Nuestra plataforma facilita
+            el proceso de préstamo, conectando a prestamistas y prestatarios de manera segura y eficiente. Explora nuestra
+            amplia gama de dispositivos disponibles y descubre cómo podemos hacer que la tecnología esté al alcance de
+            todos. ¡Bienvenido a la revolución del acceso electrónico!</p>
+
+          <br><br>
+
+        </v-card>
+
+        <!-- Agrega aquí tus campos de formulario, botones, etc. -->
+
+      </div>
+
+    </center>
+
+  </div>
 </template>
 
-<style scoped>
 
-.contenedor{
- width: 100%;
- height: 100%;
- background: url('https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.alamy.es%2Fun-conjunto-de-iconos-que-representan-varios-dispositivos-electronicos-software-y-aplicaciones-utilizados-en-la-tecnologia-moderna-image539068022.html&psig=AOvVaw05CHDAX14kpjTGtyEGNOxR&ust=1701548325612000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCPCIpM6H74IDFQAAAAAdAAAAABAi') center center fixed;
- background-size: cover;
+
+<style scoped>
+.contenedor {
+
+  width: 100%;
+
+  height: 100%;
+
+
+
 }
-.center-container {
- display: flex;
- justify-content: center;
- align-items: center;
- height: 100vh;
-}
+
+
+
+
+
+
 
 .loan-form {
- max-width: 600px;
- padding: 20px;
- border: 1px solid #ccc;
- border-radius: 8px;
- box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
- background-color: #fff;
+
+  max-width: 720px;
+
+
+
+  margin-top: 20%;
+
+  padding: 20px;
+
+  border: 1px solid #ccc;
+
+  border-radius: 8px;
+
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+
+  background-color: #a8a0a0;
+
 }
+
+
 
 h1 {
- font-size: 20px;
- margin-bottom: 16px;
+
+  font-size: 25px;
+
+  margin-bottom: 16px;
+
 }
 
+
+
 p {
- font-size: 16px;
- margin-bottom: 12px;
+
+  font-size: 20px;
+
+  margin-bottom: 10px;
+
+  width: 620px;
+
 }
 </style>
